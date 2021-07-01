3046928575b3d5a5c8f6cc9b37a7aafba6f35311
tmullin6 <tyleramullins@gmail.com>
2021-06-30 19:00:48 -0600

    Created module files for menu and contact pages

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const content = document.createElement('div');
+const nav = document.createElement('div');
+
+nav.textContent="Nav Bar";
+
+document.body.appendChild(content);
+content.appendChild(nav);
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
     mode: 'development',
 
-    entry: './src/index.js',
+    entry: {
+        
+        index: './src/index.js',
+        menu: './src/menu.js',
+        contact: './src/contact.js'
+    
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Restaurant',
+        })
+    ],
 
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
 };
