6dad645876bad00f464a61d6683dbae99fbd324d
tmullin6 <tyleramullins@gmail.com>
2021-07-08 20:36:38 -0600

    Styled front page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import renderContact from './contact.js';
 import renderMenu from './menu.js';
 import './style.less';
+import HappyCoffee from './HappyCoffee.png';
 
+const title = document.createElement('div');
 const logo = document.createElement('div');
-const nav = document.createElement('div');
+const navbar = document.createElement("div");
 const homeBtn = document.createElement('button');
 const menuBtn = document.createElement ('button');
 const contactBtn = document.createElement('button');
+const hours = document.createElement('div');
+const hoursText = document.createElement('p');
 const content = document.createElement('div');
+
+
+
 
 renderTitle();
 renderPage();
+
+
+logo.addEventListener('click', ()=>{
+    clearPageContent();
+    renderPage();
+
+});
 
 homeBtn.addEventListener('click', ()=>{
    clearPageContent();
@@ -33,11 +47,13 @@ contactBtn.addEventListener('click', ()=>{
 function renderTitle() {
 
 
-nav.classList.add("nav-bar");
-
-logo.textContent = "Yuki's Dental Stick Cafe";
-logo.classList.add('logo');
-
+title.classList.add("title");
+logo.classList.add("logo");
+navbar.classList.add("nav-bar");
+hours.classList.add('hours');
+hoursText.classList.add('hours-text');
+logo.textContent = "Yuki's Chill Time Cafe";
+hoursText.textContent= "Open Everyday 7 a.m. to 5 p.m.";
 homeBtn.textContent = "Home";
 menuBtn.textContent = "Our Menu";
 contactBtn.textContent = "Contact Us";
@@ -46,20 +62,35 @@ homeBtn.classList.add("nav-btns");
 menuBtn.classList.add("nav-btns");
 contactBtn.classList.add("nav-btns");
 
-document.body.appendChild(logo);
-document.body.appendChild(nav);
+document.body.appendChild(title);
+title.appendChild(logo);
+title.appendChild(navbar);
 
-nav.appendChild(homeBtn);
-nav.appendChild(menuBtn);
-nav.appendChild(contactBtn);
+navbar.appendChild(homeBtn);
+navbar.appendChild(menuBtn);
+navbar.appendChild(contactBtn);
+
+document.body.appendChild(hours);
+hours.appendChild(hoursText);
+
 
 };
 
 function renderPage (){
-    
-    content.textContent= "This is the body of my restaurant page, It has info about this restaurant that totally does not exist";
+    const text = document.createElement('p');
+    text.textContent= "Yuki's cafe is a chill, laid back spot to enjoy a cup of coffee, grab a pastry, and relax with friends and family. Come check us out sometime! Of course this site is completely made up and this cafe does not exist, so good luck actually finding us. Go to our contact page to \"get in touch\".";
     content.classList.add("content");
+    text.classList.add("page-text");
+
+    const icon = new Image();
+    const iconDiv = document.createElement("div");
+    iconDiv.classList.add("coffee-img");
+    icon.src=HappyCoffee;
+    icon.classList.add("coffee-icon");
     document.body.appendChild(content);
+    content.appendChild(text);
+    content.appendChild(iconDiv);
+    iconDiv.appendChild(icon);
 };
 
 function clearPageContent() {
--- src/menu.js
+++ src/menu.js
@@ -3,6 +3,7 @@ function renderMenu() {
     const menu = document.createElement('div');
     const menuList = document.createElement('ul');
     let menuItems = new Array(5);
+    let menuPrices = new Array(5);
 
    for (let i =0; i<menuItems.length; i++){
         menuItems[i]= document.createElement('li');
@@ -11,13 +12,26 @@ function renderMenu() {
         menuList.appendChild(menuItems[i]);
     };
     
+
+   for (let i =0; i<menuPrices.length; i++){
+    menuPrice[i]= document.createElement('li');
+    menuPrices[i].classList.add('menu-item');
+    menuList.appendChild(menuPrices[i]);
+};
+
     menuList.classList.add('menu');
     
-    menuItems[0].textContent = 'Hamburger';
-    menuItems[1].textContent = 'Cheeseburger';
-    menuItems[2].textContent = 'Chicken Sandwich';
-    menuItems[3].textContent = 'Hot Dog';
-    menuItems[4].textContent = 'French Fries';
+    menuItems[0].textContent = 'Drip Coffee................................... $2.50';
+    menuItems[1].textContent = 'Mocha.............................................. $2.50';
+    menuItems[2].textContent = 'Latte.............................................. $2.50';
+    menuItems[3].textContent = 'Iced Coffee................................... $2.50';
+    menuItems[4].textContent = 'Assorted Pastries............................. $2.50';
+
+    menuPrices[0].textContent= '$2.50';
+    menuPrices[1].textContent= '$2.50';
+    menuPrices[2].textContent= '$2.50';
+    menuPrices[3].textContent= '$2.50';
+    menuPrices[4].textContent= '$2.50';
 
     menu.appendChild(menuList);
     
